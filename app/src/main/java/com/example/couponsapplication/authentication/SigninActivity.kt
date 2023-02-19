c0b411c2c8c146982c44104fc5631a3cc07fb9c6
Mubashar Tariq <75364803+69mubashar@users.noreply.github.com>
2023-02-19 15:48:36 +0500

    sign-in XML

--- app/src/main/java/com/example/couponsapplication/authentication/SigninActivity.kt
+++ app/src/main/java/com/example/couponsapplication/authentication/SigninActivity.kt
@@ -3,10 +3,19 @@ package com.example.couponsapplication.authentication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.couponsapplication.R
+import com.example.couponsapplication.databinding.ActivitySigninBinding
 
 class SigninActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySigninBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin)
+
+        binding = ActivitySigninBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+
     }
 }
